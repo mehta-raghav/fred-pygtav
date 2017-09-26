03767a10017adbb084aba54481c91377acef4465
Raghav Mehta <raghav.mehta.17@gmail.com>
2017-09-26 23:06:47 +0530

    added key support for numpad
    Prakhar told me not to waste time on this.
    But does it count as time waste when I only took like 50 secs? :astonished:

--- getkeys.py
+++ getkeys.py
@@ -1,15 +1,28 @@
-# Citation: Box Of Hats (https://github.com/Box-Of-Hats)
+# Citation: Box Of Hats (https://github.com/Box-Of-Hats )
 
 import win32api as wapi
 import time
 
+def custom_ord(a):
+    if (a == '8'):
+        return 0x68
+    elif (a == '6'):
+        return 0x66
+    elif (a =='5'):
+        return 0x65
+    elif (a =='4'):
+        return 0x64
+    else:
+        return ord(a)
+
+
 keyList = ["\b"]
-for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
-    keyList.append(char)
+for _ in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
+    keyList.append(_)
 
 def key_check():
     keys = []
     for key in keyList:
-        if wapi.GetAsyncKeyState(ord(key)):
+        if wapi.GetAsyncKeyState(custom_ord(key)):
             keys.append(key)
     return keys
