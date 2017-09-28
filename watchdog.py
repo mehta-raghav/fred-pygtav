5e1b7062e21b27b4070c443cab85d071bba58ddb
Raghav Mehta <raghav.mehta.17@gmail.com>
2017-09-29 01:00:39 +0530

    Added Watchdog
    Created a watchdog script to visualize training batches.

---
+++ watchdog.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from collections import Counter
+import cv2
+
+train_data = np.load('training_data_takeoff.npy')
+
+df = pd.DataFrame(train_data)
+print(df.head())
+print(Counter(df[1].apply(str)))
+
+
+for data in train_data:
+    img = data[0]
+    print (img.shape)
+    choice = data[1]
+    cv2.imshow('test',img)
+    print (choice)
+    if cv2.waitKey(25) & 0XFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
