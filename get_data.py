ec50cfa366661b0172e8d1c9b9730caba0a6a6fc
Raghav Mehta <raghav.mehta.17@gmail.com>
2017-09-29 01:05:24 +0530

    Window handle crash fix
    Added a fix for the window handle bug and minor code cleanup.

--- get_data.py
+++ get_data.py
@@ -5,11 +5,9 @@ import time
 from getkeys import key_check
 import os
 
-#import_module(screengrab)
-
 def keys_to_output(keys):
     '''Convert keys to a multi-hot array boolean values.
-             [W,S,A,D,8,6,5,4]'''
+             [W,S,A,D,8,4,5,6]'''
     output = [0,0,0,0,0,0,0,0]
 
     if 'W' in keys:
@@ -20,15 +18,14 @@ def keys_to_output(keys):
         output[2] = 1
     if 'D' in keys:
         output[3] = 1
-    if '8' in keys:
+    if "8" in keys:
         output[4] = 1
-    if '6' in keys:
+    if "4" in keys:
         output[5] = 1
-    if '5' in keys:
+    if "5" in keys:
         output[6] = 1
-    if '4' in keys:
+    if "6" in keys:
         output[7] = 1
-
     return output
 
 def load_data(file_name):
@@ -38,7 +35,6 @@ def load_data(file_name):
     else:
         print('File does not exist, starting fresh!')
         training_data = []
-
     return training_data
 
 def countdown():
@@ -47,23 +43,32 @@ def countdown():
         time.sleep(1)
 
 def main():
-    file_name = 'training_data.npy'
+    file_name = 'training_data_takeoff'
     training_data = load_data(file_name)
-
-    paused = True
-
+    
+    paused = False
+    
     print('Initialising Data Collection in:')
     countdown()
     while True:
         if not paused:
-            screen = grab_screen()
-            screen = cv2.resize(screen, (160,120), interpolation = cv2.INTER_AREA) #resize
-            output = key_check()
-            training_data.append([screen,output])
+            try:
+                screen = grab_screen()
+                screen = cv2.resize(screen, (240,135), interpolation = cv2.INTER_AREA) #resize
+    ##            cv2.imshow('window',screen)
+    ##            if cv2.waitKey(25) & 0xFF == ord('q'):
+    ##                 cv2.destroyAllWindows()
+    ##                 break
+                output = keys_to_output(key_check())
+                print (output)
+                training_data.append([screen,output])
+                
+                if len(training_data) % 1000 == 0:
+                    print(len(training_data))
+                    np.save(file_name,training_data)
 
-        if len(training_data) % 1000 == 0:
-            print(len(training_data))
-            np.save(file_name,training_data)
+            except:
+                print ("Window not available. Please try again")
 
         keys = key_check()
         if 'T' in keys:
