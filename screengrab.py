8ed6cbd57a3bbe16a18a27bae8cbc597a7fad0b6
Raghav Mehta <raghav.mehta.17@gmail.com>
2017-09-29 01:04:08 +0530

    found dynamic border
    Now the window borders are dynamic!

--- screengrab.py
+++ screengrab.py
@@ -3,34 +3,32 @@ import numpy as np
 import win32gui, win32ui, win32con, win32api
 import time
 
-hwin = win32gui.FindWindow(None,'Grand Theft Auto V') #it gets the process ID or as microsoft calls it 'window handle'
+hwin = win32gui.FindWindow(None,'Grand Theft Auto V')#it gets the process ID or as microsoft calls it 'window handle'
+rect = win32gui.GetWindowRect(hwin)
+
 
 def grab_screen():
 
     #last_time = time.time()
     while(True):
-        #currently hardcoding the values for 800x600, have to find another way to get the values
-        top = 8
-        left = 40
-        height = 592
-        width = 792
 
+        x = rect[0]
+        y = rect[1]
+        left = 0
+        top = 40
+        height = rect[3] - y - top
+        width = rect[2] - x
+        
         hwindc = win32gui.GetWindowDC(hwin)
         srcdc = win32ui.CreateDCFromHandle(hwindc)
         memdc = srcdc.CreateCompatibleDC()
         bmp = win32ui.CreateBitmap()
         bmp.CreateCompatibleBitmap(srcdc, width, height)
         memdc.SelectObject(bmp)
-        memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
-        
+        memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY) 
         signedIntsArray = bmp.GetBitmapBits(True)
         img = np.fromstring(signedIntsArray, dtype='uint8')
         img.shape = (height,width,4)
-
-        #cv2.imshow('window',img)
-        #if cv2.waitKey(25) & 0xFF == ord('q'):
-        #     cv2.destroyAllWindows()
-        #     break
         srcdc.DeleteDC()
         memdc.DeleteDC()
         win32gui.ReleaseDC(hwin, hwindc)
@@ -38,4 +36,3 @@ def grab_screen():
         #print('loop took {} seconds'.format(time.time()-last_time))
         #last_time = time.time()
         return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
