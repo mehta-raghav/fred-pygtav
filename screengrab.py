ad6f209018a825915d92a01e580a825edfb7b2ba
Sarthak Rana <sarthak6246@gmail.com>
2020-10-01 09:52:45 +0530

    Formatted code - PEP8 style
    Formatted code in PEP8 format

--- screengrab.py
+++ screengrab.py
@@ -1,24 +1,21 @@
 import cv2
+import time
 import numpy as np
 import win32gui, win32ui, win32con, win32api
-import time
+
 
 hwin = win32gui.FindWindow(None,'Grand Theft Auto V')#it gets the process ID or as microsoft calls it 'window handle'
 rect = win32gui.GetWindowRect(hwin)
 
 
 def grab_screen():
-
-    #last_time = time.time()
     while(True):
-
         x = rect[0]
         y = rect[1]
         left = 0
         top = 40
         height = rect[3] - y - top
-        width = rect[2] - x
-        
+        width = rect[2] - x   
         hwindc = win32gui.GetWindowDC(hwin)
         srcdc = win32ui.CreateDCFromHandle(hwindc)
         memdc = srcdc.CreateCompatibleDC()
